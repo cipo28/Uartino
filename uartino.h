cf05e4f6838ce83d8a64ad67fda9775bbb7af3bd
Gianventotto <cipo28@users.noreply.github.com>
2018-10-19 22:05:32 +0200

    Update uartino.h

--- uartino.h
+++ uartino.h
@@ -1,6 +1,13 @@
+#ifndef Uartino_h
+#define Uartino_h
+
+#include "Arduino.h"
+
 class Uartino
 {
   public:
     String sendCommand(const char* command,  unsigned int delayer, unsigned int timeout);
     Boolean fchk(String value, String expectecd)
 };
+
+#endif
