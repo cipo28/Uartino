9f798756cf755d081fdc44002fc525ae5dc50ff1
Gianluca Rosi <cipo28@users.noreply.github.com>
2018-10-24 19:11:44 +0200

    AltSoftSerial support

--- uartino.h
+++ uartino.h
@@ -1,6 +1,7 @@
 class Uartino
 {
   public:
-    String sendCommand(const char* command,  unsigned int delayer, unsigned int timeout);
+    String sendCommand(SoftwareSerial mySerial, const char* command,  unsigned int delayer, unsigned int timeout);
+    String sendCommand(AltSoftSerial mySerial, const char* command,  unsigned int delayer, unsigned int timeout);
     Boolean fchk(String value, String expectecd)
 };
